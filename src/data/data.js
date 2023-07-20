ceac02bacbe8c801245cbf28b9616330b57ee81e
heba892 <hebagamal231199@gmail.com>
2023-07-20 18:28:00 +0300

    first commit

---
+++ src/data/data.js
@@ -0,0 +1,37 @@
+import BookingApp from '../assets/projects/bookingapp.jpg'
+import ShoppingCart from '../assets/projects/shoppingcart.jpg'
+import YoutubeClone from '../assets/projects/youtube.jpg'
+import QouteGenerator from '../assets/projects/qoutegenerator.jpg'
+
+export const data=[
+    {
+        id:1,
+        name:"Youtube Clone",
+        image:YoutubeClone,
+        live:"https://youtube-clone-thir.vercel.app/",
+        github:'https://github.com/heba892/youtube-clone'
+    },
+    {
+        id:2,
+        name:"Booking App",
+        image:BookingApp,
+        live:"https://booking-app-6s1j.vercel.app/",
+        github:'https://github.com/heba892/booking-app'
+    },
+    {
+        id:3,
+        name:"Shopping Cart",
+        image:ShoppingCart,
+        live:"https://soping-cart-souvinirs.vercel.app/",
+        github:'https://github.com/heba892/Soping-cart-souvinirs'
+    },
+    {
+        id:4,
+        name:"Quote Generator",
+        image:QouteGenerator,
+        live:"https://quote-generator-blue-eight.vercel.app/",
+        github:'https://github.com/heba892/Quote-generato'
+    },
+
+
+]
