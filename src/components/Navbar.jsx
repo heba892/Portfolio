b7375e6f63d9ad8ee80793fe61169ce0b12fa534
heba892 <hebagamal231199@gmail.com>
2023-08-28 00:22:21 +0300

    add resume link

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -120,7 +120,7 @@ const Navbar = () => {
           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]'>
             <a
               className='flex justify-between items-center w-full text-gray-300'
-              href='https://drive.google.com/file/d/1GX5Zb7LYM_PQys0IEjDAcGfXrxntgyk4/view?usp=sharing'
+              href='https://drive.google.com/drive/folders/1rjzS3NbDENd--dYQc922wjkIVltxdJ3Q?usp=sharing'
               target='_blank'
 
             >
